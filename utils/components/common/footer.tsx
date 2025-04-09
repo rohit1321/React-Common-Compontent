35f1a13570c2b23539129ee74861ce2be1eca596
Rohit Benibangade <benibangaderohit12@gmail.com>
2025-04-09 15:53:23 +0530

    feat!: update version

--- utils/components/common/footer.tsx
+++ utils/components/common/footer.tsx
@@ -4,8 +4,7 @@ import { version } from "../../../package.json";
 export default function Footer(){
     return (
         <>
-            <div className="d-flex justify-content-center my-4">v {version}</div>
-            
+            <div className="d-flex justify-content-center my-4">version:- {version}</div>
         </>
     );
 }
